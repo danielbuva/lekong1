429d3938b4d4a0ae3ce2e704f60f66b617eca010
danielbuva <daniel.valdecantos@gmail.com>
2023-10-29 22:18:51 -0700

    feat: add mandala loader

--- app/loaders/page.tsx
+++ app/loaders/page.tsx
@@ -1,9 +1,11 @@
+import MandalaLoader from "@/components/loaders/Mandala";
 import Link from "next/link";
 
 export default function LoaderPage() {
   return (
     <div className="min-h-screen items-center flex justify-center">
       <Link href="/">go home</Link>
+      <MandalaLoader />
     </div>
   );
 }
---
+++ components/loaders/Mandala.tsx
@@ -0,0 +1,5 @@
+export default function MandalaLoader() {
+  return (
+    <span className="w-96 h-96 rounded-full inline-block relative border-2 box-border animate-[rotation_4s_linear_infinite] after:content-[''] after:box-border after:absolute after:left-2 after:top-2 after:border-2 after:w-80 after:h-80 after:rounded-full before:content-[''] before:w-60 before:h-60 before:rounded-full before:border-2 before:absolute before:top-5 before:left-5" />
+  );
+}
