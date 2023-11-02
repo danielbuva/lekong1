a76c0ed31da611fbdfad4c2be6ae14c85e9bf556
danielbuva <daniel.valdecantos@gmail.com>
2023-11-02 16:16:20 -0700

    feat: add logo and layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,17 @@
 import Spinner from "@/components/loaders/Spinner";
 import { TrackballControls } from "@react-three/drei";
 import dynamic from "next/dynamic";
-import Link from "next/link";
-import { Suspense } from "react";
+import Image from "next/image";
+import { Suspense, useState } from "react";
 
 const VialModel = dynamic(
   () => import("@/components/canvas/models").then((mod) => mod.VialModel),
+  {
+    ssr: false,
+  }
+);
+const HerbWorkStationPlaceHolder = dynamic(
+  () => import("@/components/canvas/models").then((mod) => mod.HerbWorkStationPlaceHolder),
   {
     ssr: false,
   }
@@ -25,15 +31,17 @@ const View = dynamic(
 // );
 
 export default function Page() {
+  const [show, setShow] = useState(false)
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex-col flex min-h-screen justify-between">
       <div className="absolute flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="w-44 h-9 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"></div>
       </div>
-      <Link href="/loaders">loaders</Link>
-      <View className="min-h-screen sm:w-full">
+      <p className="z-10 select-none" onClick={()=>{setShow(!show)}}>LeKONG</p>
+      <View className="min-h-screen sm:w-full absolute top-0">
         <Suspense fallback={null}>
           <VialModel />
+          <HerbWorkStationPlaceHolder visible={show}/>
           <TrackballControls
             maxDistance={950}
             minDistance={30}
@@ -42,6 +50,9 @@ export default function Page() {
           />
         </Suspense>
       </View>
+      <div className="w-full justify-end flex">
+        <Image alt="logo" height={0} src="/lekong.png" width={60}/>
+      </div>
     </main>
   );
 }
