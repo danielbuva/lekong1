da80b5234801507a79162191f5e89d3ffe21ebde
danielbuva <daniel.valdecantos@gmail.com>
2023-11-16 14:34:13 -0800

    feat: add splash

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Splash from "@/components/dom/Splash";
 import Spinner from "@/components/loaders/Spinner";
 import LekongIconLogo from "@/components/logo/LekongIconLogo";
 import LekongNameLogo from "@/components/logo/LekongNameLogo";
@@ -26,9 +27,7 @@ export default function Page() {
 
   return (
     <main className="flex min-h-screen select-none flex-col justify-between">
-      {/* <div className="absolute z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <div className="relative h-9 w-44 dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"></div>
-      </div> */}
+      <Splash />
       <div className="z-10 flex w-full justify-between pr-6">
         <LekongNameLogo onClick={() => setShow(true)} />
         <ThemeSwitcher />
---
+++ components/dom/Splash.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/app/lib/utils";
+import { useTheme } from "@/hooks/useTheme";
+
+export default function Splash() {
+  const { theme } = useTheme();
+
+  return (
+    <div
+      className={cn(
+        "absolute z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:lg:h-[360px]",
+        {
+          "before:bg-gradient-to-br before:from-transparent before:to-blue-700 before:opacity-10 after:from-sky-900 after:via-[#0141ff] after:opacity-40":
+            theme === "dark",
+        },
+      )}
+    >
+      <div
+        className={cn("relative h-9 w-44", {
+          "drop-shadow-[0_0_0.3rem_#ffffff70] invert": theme === "dark",
+        })}
+      ></div>
+    </div>
+  );
+}
