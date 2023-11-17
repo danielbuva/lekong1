bb3fb7c74f7828037fea95e63ddaaf25d1563fd6
danielbuva <daniel.valdecantos@gmail.com>
2023-11-16 16:11:33 -0800

    style: adjust circles start position

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,8 @@ const config: Config = {
       },
       keyframes: {
         rotation: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
+          "0%": { transform: "rotate(270deg)" },
+          "100%": { transform: "rotate(-90deg)" },
         },
       },
     },
