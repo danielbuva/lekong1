f4630dc309932b3e4330835889deb7b97b35e2de
danielbuva <daniel.valdecantos@gmail.com>
2023-11-16 16:12:09 -0800

    refactor: mandala loader

--- app/loaders/page.tsx
+++ app/loaders/page.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import MandalaLoader from "@/components/loaders/Mandala";
-import Spirograph from "@/components/svgs/Spirograph";
-import Link from "next/link";
 
 export default function LoaderPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Link href="/">go home</Link>
       <MandalaLoader />
-      <div className="absolute">
-        <Spirograph />
-      </div>
     </div>
   );
 }
--- components/loaders/Mandala.tsx
+++ components/loaders/Mandala.tsx
@@ -1,5 +1,12 @@
+import Spirograph from "../svgs/Spirograph";
+
 export default function MandalaLoader() {
   return (
-    <span className="relative box-border inline-block h-96 w-96 animate-[rotation_4s_linear_infinite] rounded-full border-2 border-indigo-600 before:absolute before:left-5 before:top-5 before:h-60 before:w-60 before:rounded-full before:border-2 before:border-indigo-600 before:content-[''] after:absolute after:left-2 after:top-2 after:box-border after:h-80 after:w-80 after:rounded-full after:border-2 after:border-indigo-600 after:content-['']" />
+    <>
+      {/* <span className="relative box-border inline-block h-96 w-96 animate-[rotation_4s_linear_infinite] rounded-full border-2 border-indigo-600 opacity-30 before:absolute before:left-5 before:top-5 before:h-60 before:w-60 before:rounded-full before:border-2 before:border-indigo-600 before:content-[''] after:absolute after:left-2 after:top-2 after:box-border after:h-80 after:w-80 after:rounded-full after:border-2 after:border-indigo-600 after:content-['']" /> */}
+      <div className="absolute">
+        <Spirograph />
+      </div>
+    </>
   );
 }
